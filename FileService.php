ced6202b0d966eb48d49fbc69881ac96f62d8734
ganesh-421 <ganesh149adhikari@gmail.com>
2024-05-07 12:35:56 +0545

    fix: file service updated

--- FileService.php
+++ FileService.php
@@ -2,21 +2,38 @@
 
 namespace App\Services;
 
+use Illuminate\Http\File;
 use Illuminate\Http\UploadedFile;
+// use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class FileService
 {
     /**
-     * @param Illuminate\Http\UploadedFile $file
      * @param string $directory directory where file is to be stored
      * @return string path to the file inside storage
      */
-    public function upload(UploadedFile $file, $directory = 'uploads'): string
+    public function upload($file, $directory = 'uploads'): string
     {
-        $fileName = uniqid() . '_' . time() . '.' . $file->getClientOriginalExtension();
-        $path = $file->storeAs($directory, $fileName, 'public');
-        return $path;
+
+        if ($file instanceof UploadedFile) {
+            $fileName = uniqid() . '_' . time() . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs($directory, $fileName, 'public');
+            return $path;
+        } else if (is_string($file)) {
+
+            $imageBinary = base64_decode($file);
+
+            $finfo = finfo_open();
+            $mimeType = finfo_buffer($finfo, $imageBinary, FILEINFO_MIME_TYPE);
+            finfo_close($finfo);
+            $extension = explode('/', $mimeType)[1];
+            $fileName = uniqid() . '_' . time() . '.' . $extension;
+            $path = Storage::disk('public')->put($directory . '/' . $fileName, $imageBinary);
+            if ($path)
+                return $directory . '/' . $fileName;
+            return false;
+        }
     }
 
     /**
